5514db0e6d1d223c78b4b454907b400ca1b21ce0
zhoujunh02-del <zhou.junh02@gmail.com>
2026-03-23 19:41:56 -0400

    Add Redis caching with Cache-Aside pattern

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 import random
 import string
 from database import SessionLocal, URL
+from redis_client import redis_client
 
 app = FastAPI()
 
@@ -23,15 +25,20 @@ def shorten_url(request: ShortenRequest):
     db.add(entry)           # add to MySQL
     db.commit()             # commit
     db.close()
+    redis_client.setex(short_code, 3600, request.url)
     return {"short_url": f"http://localhost:8000/{short_code}"}
 
 
 @app.get("/{short_code}")
 def redirect_url(short_code: str):
+    orginal_url = redis_client.get(short_code)
+    if orginal_url:
+        return RedirectResponse(url=orginal_url, status_code=302)
     db = SessionLocal()
     # get first URL whose short_code == short_code
     entry = db.query(URL).filter(URL.short_code == short_code).first()
     db.close()
     if not entry:
         raise HTTPException(status_code = 404, detail = "Short URL not fount")
-    return {"original_url": entry.original_url}
+    redis_client.setex(short_code, 3600, entry.original_url)
+    return RedirectResponse(url=entry.original_url, status_code=302)
